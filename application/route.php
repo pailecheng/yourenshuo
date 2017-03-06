10145c532b542d979ed1670beee2309f0d46be35
pailecheng <981005580@qq.com>
2017-03-06 09:04:13 +0800

    3月6日

--- application/route.php
+++ application/route.php
@@ -9,13 +9,12 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
-
-];
+use think\Route;  
+Route::get('/',function(){  
+    return 'Hello,world!';  
+});  
+Route::get('news/:id','index/News/read');   //查询  
+Route::post('news','index/News/add');       //新增  
+Route::put('news/:id','index/News/update'); //修改  
+Route::delete('news/:id','index/News/delete'); //删除  
+//Route::any('new/:id','News/read');        // 所有请求都支持的路由规则  
